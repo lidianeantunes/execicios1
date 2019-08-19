113fe87e59346a9a3bf46880f0872f40caf4e5e0
lidiane <lidianeantunes346@gmail.com>
2019-08-19 16:38:05 -0300

    adicionar execicos1

---
+++ aula5.py
@@ -0,0 +1,7 @@
+lista de numeros [1,2,4,7]:
+    print('1,2,4,7') scr + (1,2,4,7):
+
+  def inpar_par ()
+ 
+
+   
---
+++ execicios 1.py
@@ -0,0 +1,7 @@
+numero = float(input('digite um numero para saber se e par ou inpar'))
+resto =numero %2
+
+if resto =0:
+    print('numero é par')
+else:
+    print('numero é inpar')
